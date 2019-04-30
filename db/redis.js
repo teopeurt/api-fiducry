9a9a21e3cda897314f235403e0be3757aea6d1d2
don <don@don.com>
2019-04-30 12:13:50 +0100

    docker update

--- db/redis.js
+++ db/redis.js
@@ -4,7 +4,16 @@ import redis from "redis";
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-const client = redis.createClient(process.env.REDIS_URL);
+
+// APPROACH 1: Using environment variables created by Docker
+// var client = redis.createClient(
+//  process.env.REDIS_PORT_6379_TCP_PORT,
+//    process.env.REDIS_PORT_6379_TCP_ADDR
+// );
+
+// APPROACH 2: Using host entries created by Docker in /etc/hosts (RECOMMENDED)
+var client = redis.createClient('6379', 'redis');
+
 
 client.on("connect", () => {
   console.log("Redis connected");
